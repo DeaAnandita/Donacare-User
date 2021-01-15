4a0a3c8346f48b8216542117e2e0eb354673e2fc
Kinanty <kinantykusuma@gmail.com>
2021-01-15 15:41:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/donacare/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.donacare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        tvRegister = (TextView) findViewById(R.id.tvRegister);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+                finish();
+            }
+        });
+    }
+}
