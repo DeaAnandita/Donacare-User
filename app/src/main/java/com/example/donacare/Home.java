074f61d3fa79176aaab3af0fa16e8b0e7b2e686b
Kinanty <kinantykusuma@gmail.com>
2021-01-23 15:54:50 +0700

    Login Firebase and Bottom Navigation

--- app/src/main/java/com/example/donacare/Home.java
+++ app/src/main/java/com/example/donacare/Home.java
@@ -14,7 +14,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        getSupportActionBar().hide();
     }
 
     public void logout(View view) {
