e0760f9a84110cdb3cc6e8aa554732bb6cf855ed
Reyhz <reyhanhammamz@gmail.com>
2021-01-28 14:59:18 +0700

    pull

--- app/src/main/java/com/example/donacare/UI/InputDanaActivity.java
+++ app/src/main/java/com/example/donacare/UI/InputDanaActivity.java
@@ -1,50 +1,138 @@
 package com.example.donacare.UI;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.donacare.Model.AccountModel;
+import com.example.donacare.Preferences;
 import com.example.donacare.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class InputDanaActivity extends AppCompatActivity {
 
     ProgressDialog progressDialog;
     Button btnKirim;
-    EditText nominal,noRek,atasNama;
+    EditText Nominal, NoRek, AtasNama;
+
+    DatabaseReference databaseReference;
+    Preferences preferences;
+    String email, name, address, phone;
+
+    Spinner spItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_dana);
 
-        btnKirim = findViewById(R.id.btn_Kirim_Dana);
-        nominal= findViewById(R.id.et_nominal_inputDana);
-        noRek=findViewById(R.id.et_noRekening_inputDana);
-        atasNama=findViewById(R.id.et_atasNama_inputDana);
 
-        String nom = nominal.getText().toString();
-        String no = noRek.getText().toString();
-        String atNam = atasNama.getText().toString();
+        preferences = new Preferences();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("users");
+
+        progressDialog = new ProgressDialog(this);
+
+        btnKirim = findViewById(R.id.btn_Kirim_Dana);
+        Nominal = findViewById(R.id.et_nominal_inputDana);
+        NoRek = findViewById(R.id.et_noRekening_inputDana);
+        AtasNama = findViewById(R.id.et_atasNama_inputDana);
+        spItem = findViewById(R.id.spPilihanDonasi);
+
+        String nominal = Nominal.getText().toString();
+        String noRek = NoRek.getText().toString();
+        String atName = AtasNama.getText().toString();
+
+        String[] ITEMS = {"Barang", "Dana", "Jasa"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ITEMS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spItem.setAdapter(adapter);
+
+        spItem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Intent intent = null;
+                if (item.equals("Barang")) {
+                    intent = new Intent(getApplicationContext(), InputDonasiActivity.class);
+                } else if (item.equals("Jasa")) {
+//                    intent = new Intent(getApplicationContext(), Input)
+                }
+                startActivity(intent);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         btnKirim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (nom.trim().isEmpty()) {
-                    nominal.setError("Kolom tidak boleh kosong!");
-                } else if(no.trim().isEmpty()) {
-                    noRek.setError("kolom tidak boleh kosong!");
-                }else if(atNam.trim().isEmpty()){
-                    atasNama.setError("kolom tidak boleh kosong!");
-                }else{
+                if (nominal.trim().isEmpty()) {
+                    Nominal.setError("Kolom tidak boleh kosong!");
+                } else if (noRek.trim().isEmpty()) {
+                    NoRek.setError("kolom tidak boleh kosong!");
+                } else if (atName.trim().isEmpty()) {
+                    AtasNama.setError("kolom tidak boleh kosong!");
+                } else {
                     progressDialog.setMessage("Sending your data...");
                     progressDialog.setCancelable(false);
                     progressDialog.show();
-                    startActivity(new Intent(InputDanaActivity.this,HomeActivity.class));
+                    Nominal.setError(null);
+                    NoRek.setError(null);
+                    AtasNama.setError(null);
+
+                    databaseReference.child(atName);
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            progressDialog.dismiss();
+                            if (snapshot.getValue() != null) {
+                                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                                }
+                                if (true) {
+                                    preferences.setName(getApplicationContext(), name);
+                                    preferences.setAddress(getApplicationContext(), address);
+//                                    preferences.setUsername(getApplicationContext(), username);
+                                    preferences.setEmail(getApplicationContext(), email);
+                                    preferences.setPhone(getApplicationContext(), phone);
+                                    preferences.setStatus(getApplicationContext(), true);
+
+                                    Log.d("email", email);
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Something is wrong..", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            progressDialog.dismiss();
+                        }
+                    });
+                    startActivity(new Intent(InputDanaActivity.this, HomeActivity.class));
                 }
             }
         });
--- app/src/main/java/com/example/donacare/UI/InputDonasiActivity.java
+++ app/src/main/java/com/example/donacare/UI/InputDonasiActivity.java
@@ -2,34 +2,45 @@ package com.example.donacare.UI;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.donacare.Preferences;
 import com.example.donacare.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.List;
 
 import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class InputDonasiActivity extends AppCompatActivity {
+public class InputDonasiActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    String id, media;
+    Spinner spItem;
     Button btn_inputFoto,btn_kirim;
-    EditText etJenis, etJumlah, etBerat, etAlamat;
+    EditText txtJenisBarang, txtJumlahBarang, txtBeratBarang, txtAlamatLengkap;
     ProgressDialog progressDialog;
     Preferences preferences;
 
@@ -54,12 +65,13 @@ public class InputDonasiActivity extends AppCompatActivity {
             EasyPermissions.requestPermissions(this, "Grant the permission to fully access this apps", 10, permissions);
         }
 
+        spItem = findViewById(R.id.spPilihanDonasi);
         btn_inputFoto = findViewById(R.id.btn_inputFoto_inputDonasi);
-        btn_kirim = findViewById(R.id.btn_Kirim);
-        etJenis = findViewById(R.id.et_Jenis);
-        etJumlah = findViewById(R.id.et_Jumlah);
-        etBerat = findViewById(R.id.et_Berat);
-        etAlamat = findViewById(R.id.et_Alamat);
+        btn_kirim = findViewById(R.id.btnKirim);
+        txtJenisBarang = findViewById(R.id.txtJenisBarang);
+        txtJumlahBarang = findViewById(R.id.txtJumlahBarang);
+        txtBeratBarang = findViewById(R.id.txtBeratBarang);
+        txtAlamatLengkap = findViewById(R.id.txtAlamatLengkap);
 
         preferences = new Preferences();
 
@@ -68,13 +80,11 @@ public class InputDonasiActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(this);
 
-        String jenis = etJenis.getText().toString();
-        String jumlah = etJumlah.getText().toString();
-        String berat = etBerat.getText().toString();
-        String alamat = etAlamat.getText().toString();
-
-
-
+        String jenis = txtJenisBarang.getText().toString();
+        String jumlah = txtJumlahBarang.getText().toString();
+        String berat = txtBeratBarang.getText().toString();
+        String alamat = txtAlamatLengkap.getText().toString();
+        
         btn_inputFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,22 +92,62 @@ public class InputDonasiActivity extends AppCompatActivity {
             }
         });
 
+        String[] ITEMS = {"Barang", "Dana", "Jasa"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ITEMS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spItem.setAdapter(adapter);
+
+        spItem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Intent intent = null;
+                if (item.equals("Dana")) {
+                    intent = new Intent(getApplicationContext(), InputDanaActivity.class);
+                } else if (item.equals("Jasa")) {
+//                    intent = new Intent(getApplicationContext(), Input)
+                }
+                startActivity(intent);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         btn_kirim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (jenis.trim().isEmpty()) {
-                    etJenis.setError("Kolom tidak boleh kosong!");
-                } else if(jumlah.trim().isEmpty()) {
-                    etJumlah.setError("kolom tidak boleh kosong!");
-                }else if(berat.trim().isEmpty()){
-                    etBerat.setError("kolom tidak boleh kosong!");
-                }else if(alamat.trim().isEmpty()){
-                    etAlamat.setError("kolom tidak boleh kosong!");
+                if (jenis.trim().isEmpty() ||jumlah.trim().isEmpty()||berat.trim().isEmpty()||alamat.trim().isEmpty()) {
+                    Toast.makeText(InputDonasiActivity.this, "Kolom tidak boleh kosong!", Toast.LENGTH_SHORT).show();
                 }else{
                     progressDialog.setMessage("Sending your data...");
                     progressDialog.setCancelable(false);
                     progressDialog.show();
-                    startActivity(new Intent(InputDonasiActivity.this,HomeActivity.class));
+
+                    fileReference = storageReference.child(media).child(id).child("profile." + getFileExtension(imageUri));
+                    fileReference.putFile(imageUri).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+                            return fileReference.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                downloadUri = task.getResult();
+                                imageUrl = downloadUri.toString();
+                                startActivity(new Intent(InputDonasiActivity.this,HomeActivity.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Edit failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -109,16 +159,12 @@ public class InputDonasiActivity extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, IMG_REQUEST);
     }
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == IMG_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-//            imageUri = data.getData();
-//            imageUrl = data.getDataString();
-////            Glide.with(getApplicationContext()).load(imageUri).fitCenter().into(img_profile);
-//        }
-//    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
 
 
     // Request Permissions
@@ -127,20 +173,20 @@ public class InputDonasiActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
-//
-//    @Override
-//    public void onPermissionsGranted (int requestCode, @NonNull List<String> perms) {
-//        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-//    }
-//
-//
-//
-//    @Override
-//    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
-//        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-//            new AppSettingsDialog.Builder(this).build().show();
-//        }
-//    }
+
+    @Override
+    public void onPermissionsGranted (int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
